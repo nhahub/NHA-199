afcbc879658a48671a1d5c04de54bcf42ed00ba1
Ahmed Gomaa <engahmedgomaa1234@gmail.com>
2025-11-29 06:46:05 +0200

    added CheckOutPage locators and variables

--- src/main/java/POMpatternPages/CheckOutPage.java
+++ src/main/java/POMpatternPages/CheckOutPage.java
@@ -5,12 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Wait;
 
 public class CheckOutPage {
-    // Locators and variables
-    String url = "https://www.selenium.dev/selenium/web/dynamic.html";
-    By revealInputButtonLocator = By.id("reveal");
-    By revealedInputLocator = By.id("revealed");
-    WebDriver driver;
-    Wait<WebDriver> wait;
-    String Username = "admin";
-    String Password = "password";
+    //  Locators
+    By firstItemLocator = By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]']");
+    By cartButtonLocator = By.xpath("//*[@id=\"shopping_cart_container\"]/a']");
+    By checkoutButtonLocator = By.xpath("//*[@id=\"checkout\"]");
+    By firstnameFieldLocator = By.xpath("//*[@id=\"first-name\"]");
+    By lastnameFieldLocator = By.xpath("//*[@id=\"last-name\"]]");
+    By zipCodeFieldLocator = By.xpath("//*[@id=\"postal-code\"]");
+    By submitButtonLocator = By.xpath("//*[@id=\"continue\"]");
+
+
+    // Variables
+    String loginPageURL = "https://www.saucedemo.com/";
+    String firstname = "Ahmed";
+    String lastName = "Abu-Madawy";
+    int zipCode = 4331;
 }
+
