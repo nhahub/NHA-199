9beb838ec844bbceb32c4601ca972a2e6352fa5c
Ahmed Gomaa <engahmedgomaa1234@gmail.com>
2025-11-29 03:54:16 +0200

    update jdk to jdk microsoft 21

--- src/main/java/POMpatternPages/CheckOutPage.java
+++ src/main/java/POMpatternPages/CheckOutPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Wait;
 
-public class CheckOutPage {
+//public class CheckOutPage {
     // Locators and variables
     String url = "https://www.selenium.dev/selenium/web/dynamic.html";
     By revealInputButtonLocator = By.id("reveal");
