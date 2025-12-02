c7c0196505a11bdb638aea32f93bfa5850544405
Ahmed Gomaa <engahmedgomaa1234@gmail.com>
2025-12-02 22:18:28 +0200

    added test cases for checkout

--- src/main/java/POMpatternPages/CheckOutPage.java
+++ src/main/java/POMpatternPages/CheckOutPage.java
@@ -15,6 +15,11 @@ public class CheckOutPage {
     By finishButtonLocator = By.id("finish");
     By successmessageLocator = By.xpath("//h2");
 
+    // New locators for cancel and error handling / overview
+    By cancelButtonLocator = By.xpath("//*[@id=\"cancel\"]");
+    By errorMessageLocator = By.xpath("//*[@id=\"checkout_info_container\"]/div/form/div[1]/div[4]/h3");
+    By overviewTitleLocator = By.xpath("//span[@class='title']");
+
     Bot bot;
 
     public CheckOutPage(Bot bot) {
@@ -29,8 +34,9 @@ public class CheckOutPage {
         bot.type(lastnameFieldLocator, last);
     }
 
+    // Accepts int zip codes and sends the correct string representation
     public void enterZip(int zip){
-        bot.type(zipCodeFieldLocator,  "zip");
+        bot.type(zipCodeFieldLocator, String.valueOf(zip));
     }
 
     public void clickContinue(){
@@ -43,5 +49,20 @@ public class CheckOutPage {
 
     public boolean isSuccessMessageDisplayed(){
         return bot.getText(successmessageLocator).contains("Thank you for your order!");
+    }
+
+    // Click the cancel button on the checkout information page
+    public void clickCancel(){
+        bot.click(cancelButtonLocator);
+    }
+
+    // Return true if an error message about missing first name is shown
+    public boolean isErrorMessageDisplayed(){
+        return bot.getText(errorMessageLocator).contains("Error: First Name is required");
+    }
+
+    // Verify that the checkout overview page is displayed (title)
+    public boolean isOnCheckoutOverview(){
+        return bot.getText(overviewTitleLocator).contains("Checkout: Overview");
     }
 }
--- src/test/java/POMpattern/CheckOutTests.java
+++ src/test/java/POMpattern/CheckOutTests.java
@@ -7,7 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CheckOutTests extends TestBaseAbstract  {
-
+//TODO: ADD logging for each step and complete the rest of the checkout tests
     @Test
     public void completeCheckoutFlow(){
         LoginPage login = new LoginPage(bot);
@@ -17,11 +17,46 @@ public class CheckOutTests extends TestBaseAbstract  {
         cart.addSauceLabsBackpackToCart();
         cart.clickOnCartIcon();
         cart.clickCheckoutButton();
-        checkout.enterFirstName("John");
-        checkout.enterLastName("Doe");
+        checkout.enterFirstName("Ahmed");
+        checkout.enterLastName("Abu-Madawy");
         checkout.enterZip(12345);
         checkout.clickContinue();
         checkout.clickFinish();
         Assert.assertTrue(checkout.isSuccessMessageDisplayed(), "Success message text did not match");
+    }
+
+    @Test
+    public void checkoutValidationMissingFirstName(){
+        LoginPage login = new LoginPage(bot);
+        CartPage cart = new CartPage(bot);
+        CheckOutPage checkout = new CheckOutPage(bot);
+
+        login.navigateToandLoginWithValidCredentials();
+        cart.addSauceLabsBackpackToCart();
+        cart.clickOnCartIcon();
+        cart.clickCheckoutButton();
+
+        // Leave first name empty
+        checkout.enterLastName("Abu-Madawy");
+        checkout.enterZip(12345);
+        checkout.clickContinue();
+
+        Assert.assertTrue(checkout.isErrorMessageDisplayed(), "Expected error message for missing first name was not displayed");
+    }
+
+    @Test
+    public void cancelCheckoutReturnsToCart(){
+        LoginPage login = new LoginPage(bot);
+        CartPage cart = new CartPage(bot);
+        CheckOutPage checkout = new CheckOutPage(bot);
+
+        login.navigateToandLoginWithValidCredentials();
+        cart.addSauceLabsBackpackToCart();
+        cart.clickOnCartIcon();
+        cart.clickCheckoutButton();
+
+        // Click cancel and verify we are back on cart page
+        checkout.clickCancel();
+        Assert.assertTrue(cart.checkCartPAgeLanding(), "User was not redirected back to the cart page after cancelling checkout");
     }
 }
